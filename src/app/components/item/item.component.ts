1a07eddc8b2a18ee29c1825b307e48daf96e6f12
joelcirillo <joelcirillo2005@gmail.com>
2023-06-30 19:38:42 -0300

    se activa en el ts el evento deleteItem

--- src/app/components/item/item.component.ts
+++ src/app/components/item/item.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { Item } from 'src/app/models/item';
 @Component({
   selector: 'app-item',
@@ -7,8 +7,12 @@ import { Item } from 'src/app/models/item';
 })
 export class ItemComponent implements OnInit{
   @Input()item: Item = new Item()
+  @Output()deleteItem:EventEmitter<Item> = new EventEmitter();
   constructor(){}
   ngOnInit(): void{
+
+  }
+  onDelete(item:Item){
 
   }
 }
